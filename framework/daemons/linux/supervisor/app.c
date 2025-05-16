73d2c9948c44ccab3c136019081a5093a7b4421f
Caupar Gu <shiwgu@qti.qualcomm.com>
2025-05-16 16:47:42 +0800

    TelAF: Support importing RW/RO cfg file to config tree
    Import the .cfg file which was bundled as [w] attribute in
    .cdef to config tree with the .cfg file name as the root
    node, but if there is a same root node name already exist
    in the tree file, the import action will stop.
    
    And if it was bundled as [r] attribute, the framework will
    keep everything under this root node same as that cfg file.
    
    Change-Id: Ic323b88380e513073ff7a27e41b3599489eef0f0

--- framework/daemons/linux/supervisor/app.c
+++ framework/daemons/linux/supervisor/app.c
@@ -2461,7 +2461,6 @@ static le_result_t CreateFileLink
         {
             char devLabel[LIMIT_MAX_SMACK_LABEL_BYTES];
             le_result_t result = devSmack_GetLabel(srcStat.st_rdev, devLabel, sizeof(devLabel));
-
             LE_FATAL_IF(result == LE_OVERFLOW, "Smack label '%s...' too long.", devLabel);
 
             if (result != LE_OK)
@@ -2496,7 +2495,6 @@ static le_result_t CreateFileLink
         // Create an empty file at the specified path.
         int fd;
         while ( ((fd = open(destPath, O_RDONLY | O_CREAT, S_IRUSR)) == -1) && (errno == EINTR) ) {}
-
         if (fd == -1)
         {
             LE_ERROR("Could not create file '%s'.  %m", destPath);
@@ -2804,22 +2802,52 @@ static le_result_t CreateLibBinLinks
 //--------------------------------------------------------------------------------------------------
 static void ImportCfgForApp
 (
-    app_Ref_t appRef,                   ///< [IN] Reference to the application object.
-    const char *cfgFile,
-    const char *cfgName,
-    bool isWritable
+    app_Ref_t appRef,     ///< [IN] Reference to the application object.
+    const char *cfgFile,  ///< [IN] Cfg file name with path.
+    const char *cfgName,  ///< [IN] Cfg name without '.cfg' extention.
+    bool isWritable       ///< [IN] Cfg bundle attribute, [r] or [w].
 )
 {
-    static char pathBuffer[LE_CFG_STR_LEN_BYTES] = "";
-    char treeRootDir[LE_CFG_STR_LEN_BYTES] = "";
-    snprintf(treeRootDir, LE_CFG_STR_LEN_BYTES, "%s:/%s", appRef->name, cfgName);
+    char treeRootDir[LE_CFG_STR_LEN_BYTES] = {0};
+    char treeNode[LE_CFG_STR_LEN_BYTES] = {0};
 
-    LE_INFO("IMPORT TREE: %s %s",pathBuffer, treeRootDir);
+    if(snprintf(treeRootDir, sizeof(treeRootDir), "%s:",
+        appRef->name) >= sizeof(treeRootDir))
+    {
+        LE_ERROR("Tree root dir '%s' is too long.", treeRootDir);
+        return;
+    }
 
+    if (snprintf(treeNode, sizeof(treeNode), "/%s", cfgName) >= sizeof(treeNode))
+    {
+        LE_ERROR("Tree node path '%s' is too long.", treeNode);
+        return;
+    }
+
+    LE_INFO("Import %s file %s.cfg to %s",isWritable? "writable":"read-only", cfgName, treeRootDir);
     le_cfg_IteratorRef_t iterRef = le_cfg_CreateWriteTxn(treeRootDir);
-    le_cfgAdmin_ImportTree(iterRef, cfgFile, pathBuffer);
-    le_cfg_CommitTxn(iterRef);
+    if (isWritable)
+    {
+        // For writable cfg, no action will be taken if the node 'cfgName' already exists.
+        if (!le_cfg_IsEmpty(iterRef, treeNode))
+        {
+            LE_WARN("Tree node: %s is exist, the importing is stopped", treeNode);
+            le_cfg_CommitTxn(iterRef);
+            return;
+        }
+    }
+    else
+    {
+        if (!le_cfg_IsEmpty(iterRef, treeNode))
+        {
+            LE_WARN("Tree node: %s is not empty, delete it first", treeNode);
+            // For read-only cfg, everything under the node 'treeNode' will always be deleted.
+            le_cfg_DeleteNode(iterRef, treeNode);
+        }
+    }
 
+    le_cfgAdmin_ImportTree(iterRef, cfgFile, treeNode);
+    le_cfg_CommitTxn(iterRef);
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -2851,7 +2879,6 @@ static void GetCfgFileName
             {
                 filename++;
             }
-            LE_INFO("filename: %s", filename);
 
             snprintf(cfgName, LIMIT_MAX_PATH_BYTES, "%s", filename);
             char *dot = strrchr(cfgName, '.');
@@ -2885,7 +2912,6 @@ static le_result_t GetBundledReadOnlySrcPath
 )
 {
     char srcPath[LIMIT_MAX_PATH_BYTES] = "";
-
     if (le_cfg_GetString(cfgIter, "src", srcPath, sizeof(srcPath), "") != LE_OK)
     {
         LE_ERROR("Source file path '%s...' for app '%s' is too long.", srcPath, app_GetName(appRef));
@@ -2945,7 +2971,6 @@ static le_result_t GetCfgFileAndToConfigTree
 )
 {
     char srcPath[LIMIT_MAX_PATH_BYTES] = "";
-    LE_INFO("GetBundledReadOnlySrcPath:");
     if (le_cfg_GetString(cfgIter, "src", srcPath, sizeof(srcPath), "") != LE_OK)
     {
         LE_ERROR("Source file path '%s...' for app '%s' is too long.",
